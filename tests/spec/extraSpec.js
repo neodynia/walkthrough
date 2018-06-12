09f801d51ca68d6f8af536217072e4a527f51106
Ryan Boris <neodynia@icloud.com>
2018-06-12 01:26:37 -0400

    docs: Clean up directory

--- tests/spec/extraSpec.js
+++
@@ -1,6 +0,0 @@
-describe ('reset', function() {
-    it('resets the position of the player after a collision or reaching the end of the screen', function() {
-        expect(this.x).toBe(this.x); 
-        expect(this.y).toBe(this.y);
-    });
-});
